e11716637a351a246607ca53bc65bc92df65521f
leoloca98 <leonardolocatelli98@gmail.com>
2021-10-25 10:30:38 +0200

    Create Api Controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+    return response()->json([
+        'students' => ['cristina', 'alessandro', 'damiano', 'rocco'],
+        'teacher' => 'Marco',
+        'total' => 32
+    ]);
+});
